6192ab2d192646375514f7d7b426bfb707296ef9
Surekaraja <43601870+Surekaraja@users.noreply.github.com>
2018-10-05 16:59:46 +0530

    sfdv
    sdas

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   {
     super(props);
     this.state = {
-      defaultValue: '#fff'      
+      defaultValue: '#ooooo'      
     };    
     this.handleChangeComplete =this.handleChangeComplete.bind(this);   
   }  
